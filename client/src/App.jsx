8273810b985fde4177a1394d6e50a7c91cb010b9
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-08 15:18:22 +0530

    app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Header from './Components/Header'
 const App = () => {
   return (
-    <div>Home Haven</div>
+    <div className='bg-[#E4E8DF] h-screen'>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes> 
+    </BrowserRouter>
+    </div>
   )
 }
 
