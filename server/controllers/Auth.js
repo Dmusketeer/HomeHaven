e57a42b1ccad04f3b6ef91e9602a965aa7f703fa
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-10 19:38:55 +0530

    create signin logic

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,14 +1,33 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/User.js";
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const Auth = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
-    console.log(newUser);
     res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access-token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
